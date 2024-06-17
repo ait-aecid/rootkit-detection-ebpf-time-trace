a6371338b0060542f62d638d8c71312e657fca2b
SilPan <leo@fairkom.net>
2024-06-17 17:34:29 +0200

    grouped bar plot for intervals per PID

--- plot.py
+++ plot.py
@@ -31,7 +31,7 @@ class Plot:
             # get the most recent output file...
             files = [f for f in os.listdir('.') if os.path.isfile(os.path.join('.', f))]
             outputs = [file for file in files if re.match(r'^output.*\.json$', file)]
-            filename = outputs[-1]
+            filename = outputs.pop()
 
         with open(filename, 'r') as file:
             json_obj = json.load(file)
@@ -49,13 +49,14 @@ class Plot:
         for pid in self.processes:
             for i in range(len(self.processes[pid]) - 1):
                 event_a = self.processes[pid][i]
-                event_b = self.processes[pid][i+1]
+                event_b = self.processes[pid][i + 1]
                 type_name = event_a.probe_point + ":" + event_b.probe_point
                 try:
                     self.intervals[type_name]
                 except KeyError:
                     self.intervals[type_name] = []
-                self.intervals[type_name].append(Interval(event_b.timestamp - event_a.timestamp, event_a, event_b, pid, event_a.tgid))
+                self.intervals[type_name].append(
+                    Interval(event_b.timestamp - event_a.timestamp, event_a, event_b, pid, event_a.tgid))
 
     def interval_type_counts(self):
         plt.barh([x for x in self.intervals.keys()], [len(x) for x in self.intervals.values()])
@@ -103,7 +104,51 @@ class Plot:
                 intervals_filtered[pid][name] += 1
 
         for i in range(16):
-            axs[int(i/4), i % 4].barh(list(intervals_filtered[list(self.processes.keys())[i]].keys()),
-                                      intervals_filtered[list(self.processes.keys())[i]].values())
+            axs[int(i / 4), i % 4].barh(list(intervals_filtered[list(self.processes.keys())[i]].keys()),
+                                        intervals_filtered[list(self.processes.keys())[i]].values())
         fig.savefig("interval_types_per_run_" + self.file_date + ".svg")
         fig.clf()
+
+    def interval_types_per_run2(self):
+        def get_or0(dictionary, outer, inner):
+            try:
+                return dictionary[outer][inner]
+            except KeyError:
+                return 0
+        import numpy as np
+        intervals_filtered = {}
+        print(str(len(self.processes)))
+        for name, intervals in self.intervals.items():
+            for elem in intervals:
+                pid = elem.pid
+                if pid not in list(self.processes.keys()):
+                    pass
+                try:
+                    intervals_filtered[name]
+                except KeyError:
+                    intervals_filtered[name] = {}
+                try:
+                    intervals_filtered[name][pid]
+                except KeyError:
+                    intervals_filtered[name][pid] = 0
+                intervals_filtered[name][pid] += 1
+
+        groups = list(intervals_filtered.keys())
+        categories = list(intervals_filtered[groups[0]].keys())
+        n_groups = len(groups)
+        n_categories = len(categories)
+        fig, ax = plt.subplots()
+        bar_width = 0.05
+        index = np.arange(n_groups)
+        for i, category in enumerate(categories):
+            values = [get_or0(intervals_filtered, group, category) for group in groups]
+            bar_positions = index + i * bar_width
+            ax.bar(bar_positions, values, bar_width, label=category)
+        ax.set_xlabel('Interval Name')
+        ax.set_ylabel('Interval Measure Count')
+        ax.set_xticks(index + bar_width * (n_categories / 2 - 0.5))
+        ax.set_xticklabels(groups, rotation=90)
+        plt.tight_layout()
+
+        plt.savefig("interval_types_per_run2_" + self.file_date + ".svg")
+        plt.clf()
