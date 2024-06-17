244b51f975059ed24c592e653cd6b749fa275b97
SilPan <leo@fairkom.net>
2024-06-17 13:59:17 +0200

    make plot.py Object Oriented

--- plot.py
+++ plot.py
@@ -6,43 +6,8 @@ import re
 import sys
 from data_classes import Event
 
-try:
-    filename = sys.argv[1]
-except IndexError:
-    # get the most recent output file...
-    files = [f for f in os.listdir('.') if os.path.isfile(os.path.join('.', f))]
-    pattern = r'^output.*\.json$'
-    outputs = [file for file in files if re.match(pattern, file)]
-    filename = outputs[-1]
 
-with open(filename, 'r') as file:
-    json_obj = json.load(file)
-    events = [Event(**elem) for elem in json_obj]
-
-file_date = filename.replace("output", "").replace(".json", "")
-
-processes = {}
-for event in events:
-    try:
-        processes[event.pid]
-    except KeyError:
-        processes[event.pid] = []
-    processes[event.pid].append(event)
-
-intervals = {}
-for pid in processes:
-    for i in range(len(processes[pid]) - 1):
-        event_a = processes[pid][i]
-        event_b = processes[pid][i+1]
-        type_name = event_a.probe_point + ":" + event_b.probe_point
-        try:
-            intervals[type_name]
-        except KeyError:
-            intervals[type_name] = []
-        intervals[type_name].append(event_b.timestamp - event_a.timestamp)
-
-
-def unique_vals(lst: list) -> int:
+def __unique_vals__(lst: list) -> int:
     values = []
     unique = 0
     for elem in lst:
@@ -52,25 +17,68 @@ def unique_vals(lst: list) -> int:
     return unique
 
 
-plt.barh([x for x in intervals.keys()], [len(x) for x in intervals.values()])
-plt.tight_layout()
-plt.savefig("interval_distribution_" + file_date + ".svg")
+class Plot:
+    args = []
+    processes = {}
+    intervals = {}
+    file_date = ""
 
+    def __init__(self, args):
+        self.args = args
+        try:
+            filename = args[1]
+        except IndexError:
+            # get the most recent output file...
+            files = [f for f in os.listdir('.') if os.path.isfile(os.path.join('.', f))]
+            outputs = [file for file in files if re.match(r'^output.*\.json$', file)]
+            filename = outputs[-1]
 
-def make_histogram(name: str, values: [int]) -> None:
-    plt.hist(values, bins=unique_vals(values))
-    plt.title(name)
-    plt.yscale('log')
-    plt.savefig("distribution_" + file_date + "_" + name + ".svg")
-    print(name + " saved")
+        with open(filename, 'r') as file:
+            json_obj = json.load(file)
+            events = [Event(**elem) for elem in json_obj]
 
+        self.file_date = filename.replace("output", "").replace(".json", "")
 
-plot_processes = []
+        for event in events:
+            try:
+                self.processes[event.pid]
+            except KeyError:
+                self.processes[event.pid] = []
+            self.processes[event.pid].append(event)
 
-for name, values in intervals.items():
-    worker = Process(target=make_histogram, args=[name, values])
-    worker.start()
-    plot_processes.append(worker)
+        for pid in self.processes:
+            for i in range(len(self.processes[pid]) - 1):
+                event_a = self.processes[pid][i]
+                event_b = self.processes[pid][i+1]
+                type_name = event_a.probe_point + ":" + event_b.probe_point
+                try:
+                    self.intervals[type_name]
+                except KeyError:
+                    self.intervals[type_name] = []
+                self.intervals[type_name].append(event_b.timestamp - event_a.timestamp)
 
-for worker in plot_processes:
-    worker.join()
+    def interval_type_counts(self):
+        plt.barh([x for x in self.intervals.keys()], [len(x) for x in self.intervals.values()])
+        plt.tight_layout()
+        plt.savefig("interval_distribution_" + self.file_date + ".svg")
+        plt.clf()
+
+    def distributions(self):
+        def make_histogram(name: str, values: [int]) -> None:
+            plt.hist(values, bins=__unique_vals__(values))
+            plt.title(name)
+            plt.yscale('log')
+            plt.tight_layout()
+            plt.savefig("distribution_" + self.file_date + "_" + name + ".svg")
+            print(name + " saved")
+            plt.clf()
+
+        plot_processes = []
+
+        for name, values in self.intervals.items():
+            worker = Process(target=make_histogram, args=[name, values])
+            worker.start()
+            plot_processes.append(worker)
+
+        for worker in plot_processes:
+            worker.join()
