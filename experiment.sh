3c7c13fd4a63648fe336853faada289a49c4d971
SilPan <leo@fairkom.net>
2024-06-18 15:37:50 +0200

    introduce experiment.sh

---
+++ experiment.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -euo pipefail
+
+scp user.py kernel.c data_classes.py ubuntu2204:
+
+ssh ubuntu2204 "sudo python3 user.py -r 1000"
+
+newest_output=$(ssh ubuntu2204 "ls -1 output*" | tail -n 1)
+
+scp ubuntu2204:"$newest_output" ./
+
+python run.py "$newest_output"
