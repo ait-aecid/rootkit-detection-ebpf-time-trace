8ed2fb37acd82fb6e27d2485779ae4a4868b2d16
SilPan <leo@fairkom.net>
2024-08-14 12:58:32 +0200

    use ls-basic

--- experiment.sh
+++ experiment.sh
@@ -6,10 +6,11 @@ TARGET=ubuntu2204_linux6.x
 
 scp user.py kernel.c data_classes.py linux.py $TARGET:
 
-ssh $TARGET "sudo python3 user.py -i 10 -e ./getpid_opendir_readdir_root"
+ssh $TARGET "sudo python3 user.py -i 100 -e ./open_getdents"
 
 newest_output=$(ssh $TARGET "ls -1 output*" | tail -n 1)
 
 scp $TARGET:"$newest_output" ./
 
+source venv/bin/activate
 python run.py "$newest_output"
