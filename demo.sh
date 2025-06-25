6427998b3af3f8c4737ff0d46baf13320b7c8de3
Ubuntu <max.landauer@ait.ac.at>
2025-06-25 07:11:45 +0000

    Update demo scripts with ann and svm

--- demo.sh
+++ demo.sh
@@ -1,4 +1,8 @@
-python3 evaluate.py -d events -t 0.333 -s 42 -q 9 -r 100 -m offline -g seq
-python3 evaluate.py -d events -t 0.333 -s 42 -q 9 -r 1 -m online -g seq
-python3 evaluate.py -d events -t 0.333 -s 42 -q 9 -r 100 -m offline -g fun
-python3 evaluate.py -d events -t 0.333 -s 42 -q 9 -r 1 -m online -g fun
+python3 evaluate.py -d events -t 0.333 -s 42 -r 100 -m offline -g fun -a shift -q 9
+python3 evaluate.py -d events -t 0.333 -s 42 -r 100 -m offline -g seq -a shift -q 9
+python3 evaluate.py -d events -t 0.333 -s 42 -r 100 -m offline -g fun -a ann
+python3 evaluate.py -d events -t 0.333 -s 42 -r 100 -m offline -g seq -a ann
+python3 evaluate.py -d events -t 0.333 -s 42 -r 100 -m offline -g fun -a svm
+python3 evaluate.py -d events -t 0.333 -s 42 -r 100 -m offline -g seq -a svm
+python3 evaluate.py -d events -t 0.333 -s 42 -r 1 -m online -g fun -a shift -q 9
+python3 evaluate.py -d events -t 0.333 -s 42 -r 1 -m online -g seq -a shift -q 9
